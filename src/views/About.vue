6732839c2e25a4a0bd25b8481dcb63b4fc0fc3c2
Stephen Studer <stephenkstuder@gmail.com>
2020-08-09 10:38:37 -0700

    basic code running, CRUD works

---
+++ src/views/About.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>This is a Vue todo list app that uses axios and json placeholder to showcase a todolist.</p>
+  </div>
+</template>
+© 2020 GitHub, Inc.

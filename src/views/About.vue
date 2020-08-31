b8a3f8d71dc32d358fcc0b767984b3ee0344dcf6
Stephen Studer <stephenkstuder@gmail.com>
2020-08-31 07:03:04 -0700

    image updates

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,15 +1,29 @@
 <template>
   <div class="about bg-grey d-flex align-center flex-column">
-    <h1>About</h1>
+    <br />
+    <h1>About This Project</h1>
     <div class="content">
+      <br />
       <p>This started out as my first Vue project ever had a very plain style as shown below</p>
       <br />
-      <!-- Todo, add image and base url -->
-      <!-- Todo, deploy to gh pages -->
+      <br />
+      <v-img :src="image" alt="screenshot" class="img" />
+      <br />
+      <br />
       <p>I have used this as an example app to learn Vuetify, transitions, and will eventually create a Deno backend to support it. Some ambitions down the pipline including login, statistics, dates, etc. I am not trying to create a groundbreaking new app, i just want to showcase what i can build.</p>
     </div>
   </div>
 </template>
+<script>
+import image from "../assets/vue-screenshot.png";
+export default {
+  data: function () {
+    return {
+      image: image,
+    };
+  },
+};
+</script>
 <style>
 .bg-grey {
   background-color: #343434;
